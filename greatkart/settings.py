dc4827f3cd92ef798e780a9e1b87b553ed0d9adb
unnamedKnight <backfrontdev47@gmail.com>
2022-07-16 22:12:23 +0600

    hide django secret key

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lt*+-u0q2=nvv7x)0gg@oqya$#=&tbm04^&w013ew0627z)ah1'
+#getting the secret key from dotenv
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
