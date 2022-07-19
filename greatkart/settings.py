cd24f24913bba7324a42c91bc96d6a437c97ed98
unnamedKnight <backfrontdev47@gmail.com>
2022-07-19 19:45:57 +0600

    Add postgres as DB

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = "greatkart.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "GreatKart",
+        "USER": 'postgres',
+        "PASSWORD": "superuser",
+        "HOST": "localhost",
     }
 }
 
